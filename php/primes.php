c1d7340a6d6aa1dd0ee3a711afcbdcb184b56b83
alexdevprojects <alexdevprojects@gmail.com>
2015-06-15 01:55:41 -0700

    've learned a lot in the last 10 years, specially since I started coding for a living. Got this script out and updated it with better input filtering, code layout improvements, html5 enhancements. Most notable is a much improved calculation method that limits the divisions to 1/2 of the number being checked (as anything bellow that would return a fractional number) and most importantly it now uses modulos, also eliminating the need for the additional type checking. Benchmark with new calculation method (2.8GHz Intel Core i7) 1 - 1,000 test: Before: Processing time: 0.0402719974518 seconds (78,022 operations). Now: Processing time: 0.0045177936554 seconds (40,043 operations) (10 times faster!) 1 - 10,000 test: Before: Processing time: 2.5108559131622 seconds (5,775,223 operations). Now: Processing time: 0.4331419467926 seconds (2,907,640 operations). 1 - 100,000 test: Before: Not happening. Now: Processing time: 38.5100219249725 seconds (227,995,678 operations).

--- php/primes.php
+++ php/primes.php
@@ -1,257 +1,205 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?php
+####### DEFAULTS: #######
+$default = array('start' => 1, 'finish' => 1000);
+$limit = 100000;
+set_time_limit(120);
+?>
+<!DOCTYPE html>
 <html>
-<head>
-<title>Alexnow.com :: Prime numbers tool</title>
-</head>
+  <head>
+    <title>Alexnow.com :: Prime number finder tool</title>
+  </head>
 
-<body>
+  <body>
+    <form name="form1" method="post">
+
+      <fieldset>
+        <legend>Get all prime numbers between</legend>
+        <input type="number" name="start" value="<?= $default['start'] ?>" min="1" max="<?= $limit - 1 ?>" /> and
+        <input type="number" name="finish" value="<?= $default['finish'] ?>" min="2" max="<?= $limit ?>" /> (Maximum <?= number_format($limit) ?>) <br />
+        <input type="submit" name="submit" value="Submit" onclick="this.value='Processing...';" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Options:</legend>
+        Layout:
+        <input type="radio" name="layout" value="v" checked /> Vertical | <input type="radio" name="layout" value="h" /> Horizontal | <input type="radio" name="layout" value="t" /> Textarea<br />
+        <input type="checkbox" name="return_nonprimes" /> Also return a list of composite (non prime) numbers <br />
+        <input type="checkbox" name="numformat" /> Format numbers (i.e. 12,345) <br />
+      </fieldset>
+
+    </form>
 
 <?php
 
-####### DEFAULTS: #######
-
-$start = 1;
-$endnum = 10000;
-$max_exec_time = 120;
-$numformat = FALSE;
-$layout = 'v';
-$maxOutsideNumber = 1000000; // set to higher than usual due to outside IP validation broken
-$maxoutside_f = number_format($maxOutsideNumber);
-
-
-
-$form1 = <<<EOF
-<form name="form1" action="$_SERVER[PHP_SELF]" method="post">
-Get all primes between <br />
-<input type="text" name="start" />
-and <br>
-<input type="text" name="end" /> (Maximum $maxoutside_f) <br />
-Layout: 
-<input type="radio" name="layout" value="v" checked /> Vertical | <input type="radio" name="layout" value="h" /> Horizontal | <input type="radio" name="layout" value="t" /> Textarea<br />
-<input type="checkbox" name="return_nonprimes" /> Return non primes also <br />
-<input type="checkbox" name="numformat" /> Format numbers (x,xxx) <br />
-<input type="submit" name="submit" value="Submit" />
-EOF;
-
-if(isset($_GET['override'])){
-$form1 .= '<input type="hidden" name="outside_override" value="TRUE" />' . "\n";
-}
-
-$form1 .= '</form>';
-
-$info1 = <<<EOF
-<pre>
-Function:
-Finds all prime numbers within a range of 2 given values
-
-Method logic:
-An outter FOR loop iterates through all whole numbers, one by one, from the minimun input to the maximun. 
-
-An inner loop performs a division with the current key as dividend by all numbers from 2 to key minus 1 as divisors.
-Each result is tested to see if it's an integer, if it is, the number being procesed
-is flagged as a non-prime an stored in an array variable. 
-The inner loop is also broken since the number has already been proved to be non-prime
-(ie number 6 is divided by 2, never makes it to be divided by 3)
-
-When the outter loop finishes a temporary array is generated with the range of the 
-two original values and then it's intersected with the array containing the non-prime numbers...
-The result is an array of pure primes that gets output.
- 
-Changes:
-
-7/29/2013
-
-Hello PHP! it's been a long time. Anyway, debugged this thing and made it work, some bad var names and stuff.
-
-6/27/05
-
-Revisited this old script and made a few improvements:
-
-Presentation:
-Added option for vertical or horizontal layout.
-Added option to format the numbers returned by the script.
-Cleaned up markup (very basic to begin with) now 4.01 Transitional Valid (W3C DTD).
-
-Performance:
-Added the option to return a list of non-prime numbers and the first divisor that returns an integer.
-Better benchmarking algorithm.
-
-The new P4 server ran the 1-1000 task (78,022 ops) in 0.1383790969849 seconds when tested. 
-(even whith the new non-prime/div array)
-
-Security:
-Added some input verification (input non empty, is an integer, min < max).
-Limited max number argument to $maxoutside_f when script is called from an IP outside my network.
-   Did this to launch live, since a very large calculation would take too many valuable resources from my server.
-   Should you have the need for a calculation outside that range simply email me, tell me what you need the info for
-   and I'll run this script for you.
-
-4/30/02
-Had the great idea for the inner loop to start from 2 and continue the divisions
-through x - 1.
-(it used to be x-1 and substracting by 1 through number 2)
-
-The result was great, in the benchmark for finding primes 1 - 1000:
-Old script: Process time: 11.336248993874 seconds (335052 operations)
-New script: Process time: 2.8276569843292 seconds (78022 operations)
-
-(ie 700 had to be divided by 699, 698, 697... untill 350 where it returned 2,
-now it is divided by 2 first returnin non-prime right away)
-
-Do not ask why I originally did it the other way.
-
-Change 2:
-I made the script override PHP's default maximun execution time of 30 seconds.
-The new value is 2 minutes, just to prevent the script to "time out" during an
-unnecesary calculation of something like primes 1 to 1,000,000. 
-</pre>
-EOF;
-
-#                                     Copied this to further down as to always show the form regardles of SUBMIT 
-# if(!isset($_POST["submit"])){
-
-echo($form1);
-#echo("\n<hr />\n");
-# echo($info1);                       added this to the end of document regarless of SUBMIT
-
-
-if(!isset($_POST["submit"])){
-  echo("\n");                         //just throw something out there
-}else{ //form submitted
+if(filter_input(INPUT_POST, 'submit')){ //form submitted
 
 ####### VALIDATION #######
 
-if(isset($_POST['layout'])){
-$layout = $_POST['layout'];
-}
+  $layout = filter_input(INPUT_POST, 'layout');
+  $numformat = !is_null(filter_input(INPUT_POST, 'numformat')) ? 1 : 0;
 
-if(isset($_POST['numformat'])){
-$numformat = TRUE;
-}
+  $start = filter_input(INPUT_POST, 'start', FILTER_VALIDATE_INT, array("min_range"=>1, "max_range"=>($limit-1)));
+  $finish = filter_input(INPUT_POST, 'finish', FILTER_VALIDATE_INT, array("min_range"=>($start+1), "max_range"=>$limit));
 
+  $return_nonprimes = !is_null(filter_input(INPUT_POST, 'return_nonprimes')) ? 1: 0;
 
-if(isset($_POST['start']) && is_numeric($_POST['start']) && $_POST['start'] > 0){
-$start = $_POST['start'];
-settype($start, "int");
-}else{
-  $start = 1;
-}
+  // Handle invalid or empty inputs
+	$start = ($start == false || is_null($start)) ? $default['start'] : $start;
+  $finish = ($finish == false || is_null($finish)) ? $default['finish'] : $finish;
 
-if(isset($_POST['end']) && is_numeric($_POST['end']) && $_POST['end'] > $_POST['start']){
-$end = $_POST['end'];
-settype($end, "int");
-
-##                                                        TRY TO FIX THIS FOR SECURITY
-#	if($REMOTE_ADDR != "127.0.0.1" && !ereg("^192.168+",$REMOTE_ADDR) && !isset($_POST['outside_override']) && $end > $max_outside){
-#	$end = $max_outside;
-
-##                                                        USING THIS INSTEAD FOR NOW:
-	if($end > $maxOutsideNumber){
-	$end = $maxOutsideNumber;
-	}
-
-}else{
-  $end = 100;
-}
-
-
-set_time_limit($max_exec_time);
+// Prevent outside IPs from requesting a huge calculation
+  if(($_SERVER['REMOTE_ADDR'] != "127.0.0.1" && !ereg("^192.168+",$_SERVER['REMOTE_ADDR'])) && $finish > $limit){
+	   $finish = $limit;
+     echo "   <p><strong>Request too large, limiting calculations to ". number_format($finish) . "</strong></p>\n";
+  }
 
 ####### BENCHMARK #######
-
-$start_micro = explode(" " , microtime());
-$start_time = $start_micro[1] + $start_micro[0];
+$time_start = microtime(true);
 $operations = 0;
 
-
-
 ####### CALCULATIONS #######
+for($x = $start; $x <= $finish; $x++){
+  $last_div = $x / 2; // round()?
+   for($y = 2; $y <= $last_div; $y++){
+     $operations++;
+     if($x % $y === 0) { // $x is divisible by y without a residual and therefore not prime
+       $not_prime_array[] = $x;
+       $not_prime_divby[] = $y;
+      break 1;
+    }
 
-for($x=$start; $x<=$end; $x++){
-   for($y=2;$y < $x; $y++){
-   $current_div = $x / $y;
-   $operations++;
-   #print("$x divided by $y = $current_div<br />\n");
-
-   if(is_int(($current_div))){ // $x is not prime
-   $not_prime_array[] = $x;
-   $not_prime_divby[] = $y;
-   break 1;
    }
-   
-   }
-}
+ }
 
+  ####### BENCHMARK #######
+  $time_end = microtime(true);
+  $time_elapsed = $time_end - $time_start;
 
-
-####### BENCHMARK #######
-$end_micro = explode(" " , microtime());
-$end_time = $end_micro[1] + $end_micro[0];
-
-
-$original_values = range($start,$end);
-
-if(isset($not_prime_array)){
-$primearray = array_diff($original_values,$not_prime_array);
-}
-
-
-$process_time = $end_time - $start_time;
-
-if(isset($primearray)){
-
-print("These primes found: ($start ... $end) | Process time: " . number_format($process_time , 13 , '.' , ',') . ' seconds (' . number_format($operations) . " operations).<br/ >\n");
-
-
-switch($layout){
-  case 'h':
-    foreach($primearray as $prime){
-      print($numformat)?number_format($prime):$prime;
-      print " \n";
-    }
-  break;
-
-  case 'v':
-    foreach($primearray as $prime){
-      print($numformat)?number_format($prime):$prime;
-      print " <br />\n";
-    }
-  break;
-  
-  default:
-      print "<form>\n<textarea rows=\"20\" cols=\"10\" wrap=\"hard\">\n";
-    foreach($primearray as $prime){
-      print($numformat)?number_format($prime):$prime;
-      print "\n";
-    }
-      print "\n</textarea></form>\n";
-  break;  
-
-}
-
-
-if(isset($_POST["return_nonprimes"])){
-print "\n<hr />\nNon primes and first divisor that returns an integer: <br />\n";
-
-for($x=0; $x < count($not_prime_array); $x++){
- print $not_prime_array[$x] . ' div by ' . $not_prime_divby[$x] . "<br />\n";
-}
-
-#foreach($not_prime_array as $key => $val){ // this needed a change inside the loop creating the arrays
-#print $key . ' div by ' . $value . "<br />\n";
-#}
-
-}
-
-
-}else{ // no $primearray
-print("No primes found ($start ... $end)");
-}
-
-
-} // submit
-
-print "\n<hr />\n".$info1;
+  $primearray = array_diff(range($start,$finish),$not_prime_array);
 ?>
-</body>
+
+    <fieldset>
+      <legend>Info:</legend>
+        <p>Searching for prime numbers between <?= $start ?> and <?= $finish ?>...</p>
+        <p>Processing time: <?= number_format($time_elapsed , 13 , '.' , ',') ?> seconds (<?= number_format($operations) ?> operations).</p>
+    </fieldset>
+
+    <fieldset>
+      <legend>Results:</legend>
+<?php
+
+  if(!empty($primearray)){
+
+  echo number_format(count($primearray)) . " prime numbers found: <br />\n";
+
+
+    switch($layout){
+      case 'h':
+        foreach($primearray as $prime){
+          echo ($numformat) ? number_format($prime) : $prime;
+          echo " \n";
+        }
+      break;
+
+      case 'v':
+        foreach($primearray as $prime){
+          echo ($numformat) ? number_format($prime) : $prime;
+          echo " <br />\n";
+        }
+      break;
+
+      default:
+          echo "<form>\n<textarea rows='20' cols='10' wrap='hard'>\n";
+        foreach($primearray as $prime){
+          echo ($numformat) ? number_format($prime) : $prime;
+          echo "\n";
+        }
+          echo "\n</textarea></form>\n";
+      break;
+
+    }
+
+
+    if($return_nonprimes){
+?>
+    <hr />
+    Composite numbers and first divisor without a residual: <br />
+
+<?php
+      for($x=0; $x < count($not_prime_array); $x++){
+        echo $not_prime_array[$x] . ' div by ' . $not_prime_divby[$x] . "<br />\n";
+      }
+    }
+
+
+  } else { // empty $primearray
+    echo "No prime numbers found";
+  }
+?>
+
+    </fieldset>
+
+<?php
+} // submit
+?>
+
+    <hr />
+    <fieldset>
+      <legend>About this tool:</legend>
+      <p><strong>Finds all prime numbers within a range of 2 values.</strong></p>
+
+      Change log:
+      <dl>
+        <dt>6/14/2015</dt>
+        <dd><p>I've learned a lot in the last 10 years, specially since I started coding for a living.<br />
+          Got this script out and updated it with better input filtering, code layout improvements, html5 enhancements.<br />
+          Most notable is a much improved calculation method that limits the divisions to 1/2 of the number being checked (as anything bellow that would return a fractional number) and most importantly it now uses <strong>modulos</strong>, also eliminating the need for the additional type checking.<br />
+          <p>Benchmark with new calculation method (2.8GHz Intel Core i7)<br />
+          <em>1 - 1,000 test:</em><br />
+           Before: Processing time: 0.0402719974518 seconds (78,022 operations). <br />
+           Now: Processing time: 0.0045177936554 seconds (40,043 operations) (10 times faster!) <br />
+          <em>1 - 10,000 test:</em><br />
+           Before: Processing time: 2.5108559131622 seconds (5,775,223 operations). <br />
+           Now: Processing time: 0.4331419467926 seconds (2,907,640 operations). <br />
+          <em>1 - 100,000 test:</em><br />
+           Before: Not happening. <br />
+           Now: Processing time: 38.5100219249725 seconds (227,995,678 operations).</p>
+
+           <p><strong>To do:</strong> rewrite this in Javascript and remove the limit.</p>
+        </dd>
+
+        <!-- <dt>7/29/2013</dt>
+        <dd>Found this old script and debugged it, a lot.</dd> -->
+
+        <dt>6/27/2005</dt>
+        <dd><p>Added functionality and other improvements:<br />
+          <em>Presentation:</em><br />
+          Added option for vertical or horizontal layout.<br />
+          Added option to format the numbers returned by the script.<br />
+          Cleaned up markup (very basic to begin with) now 4.01 Transitional Valid (W3C DTD).<br />
+          <br />
+          <em>Performance:</em><br />
+          Added the option to return a list of non-prime numbers and the first divisor that returns an integer.<br />
+          Better benchmarking.<br />
+          The new P4 server ran the 1-1000 task (78,022 ops) in 0.1383790969849 seconds when tested.
+          (even whith the new non-prime/div array)<br />
+          <br />
+          <em>Security:</em><br />
+          Added some input validation (input non empty, is an integer, min < max).<br />
+          Limited the target number when script is called from an outside network.</p>
+        </dd>
+
+        <dt>4/30/2002</dt>
+        <dd><p>Changed the inner loop to start from 2 and count up to x - 1. (it used to start at x-1 and counted down to 2)
+          This greatly improved performance, in the benchmark for finding primes 1 - 1000:<br />
+          Old script: Process time: 11.336248993874 seconds (335052 operations)<br />
+          New script: Process time: 2.8276569843292 seconds (78022 operations)<br />
+          (for example: 100 had to be divided by 99, 98... until it got to 50 where it returned 2, now it returns non-prime with the very first division by 2)<br />
+          Don't ask me why I originally did it the other way.<br />
+          Increased script execution timeout to 2 minutes.</p>
+        </dd>
+      </dl>
+    </fieldset>
+  </body>
 </html>
